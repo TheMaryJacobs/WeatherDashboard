fb9d711961585c4f287be2511d1ae478b0c49dde
Mary Jacobs <TheMaryJacobs@gmail.com>
2019-11-25 22:45:50 -0600

    HTML rearrange, date formatting

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,57 @@
-const apiKey = "ee316b84f88a186c92a6e31cd2dad1d8";
+const apiKey = "0b1ad7993fb1635feccbffde378e7b49";
 let cityArray = []
 
+const TodayIs = moment().format('MMMM Do , YYYY');
+    $('#todayIs').text(TodayIs);
+
+const DayTwo = moment().add(1, 'days').calendar();
+    $('#day-Two').text(DayTwo);
+
+const DayThree = moment().add(2, 'days').calendar();
+    $('#day-Three').text(DayThree);
+    
+const DayFour = moment().add(3, 'days').calendar();
+    $('#day-Four').text(DayFour);
+    
+const DayFive = moment().add(4, 'days').calendar();
+    $('#day-Five').text(DayFive);
+
+const DaySix = moment().add(5, 'days').calendar();
+    $('#day-Six').text(DaySix); 
 
 $("#searchButton").on("click", function () {
     city = $(this).parent("div").children("div").children("input").val();
     $(this).parent("div").children("div").children("input").val("");
     currentCall();
+
+    
 });
 
+storedCities = JSON.parse(localStorage.getItem("cities"));
+
+
+if (storedCities !== null) {
+    city = storedCities[0].name;
+    window.onload = currentCall(city)
+};
+
+
+function renderList() {
+    Object.values(storedCities).forEach((value) => {
+        const $cityLi = $("<li>", { "class": "list-group-item" });
+        $cityLi.text(value.name);
+        $(".list-group").prepend($cityLi);
+    }
+    )
+}
+
+if (storedCities !== null) {
+    renderList();
+}
+
+
 function currentCall() {
+    // $('#todayIs').removeAttr("d-none");
     const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     $.ajax({
         url: queryURL,
@@ -16,15 +59,13 @@ function currentCall() {
     })
     //promise - call the api for the information
         .then(function (response) {
+            const $cityLi = $("<li>", { "class": "list-group-item" });
             //used stack exchange to figure out icon image: https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
             const iconCode = response.weather[0].icon;
             const iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png";
 
-            cityObject = {
-                name: response.name
-            }
+            city = {name: response.name}
         
-
             cityLat = response.coord.lat;
             cityLong = response.coord.lon;
             cityId = response.id;
@@ -42,13 +83,16 @@ function currentCall() {
             })
                 .then(function (response) {
                     $(".uvIndex").text("UVI: "+response.value);
-                    let $dateHeader = $("<h2>");
+                    // let $dateHeader = $("<h2>");
                     let shortDate = response.date_iso.substr(0, response.date_iso.indexOf('T'));
-                    $dateHeader.text(shortDate);
-                    $("h1").append($dateHeader);
+                    "https://openweathermap.desk.com/customer/portal/questions/17064492-date-and-time-specific-response?t=535697"
+                    // $dateHeader.text(shortDate);
+                    // $("h1").append($dateHeader);
                 })
 
+
             const fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&id=${cityId}&units=imperial`;
+            //sets the information 
             let index = 3;
             $.ajax({
                 url: fiveDayURL,
@@ -59,8 +103,9 @@ function currentCall() {
                     for (let i = 4; i < response.list.length; i += 8) {
                         const iconCode = response.list[i].weather[0].icon;
                         const iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png";
-                        const shortDate = response.list[i].dt_txt.substr(0, response.list[i].dt_txt.indexOf(' '));
-                        $("#day-" + index).text(shortDate);
+                        // const shortDate = response.list[i].dt_txt.substr(0, response.list[i].dt_txt.indexOf(' '));
+                        // $("#day-" + index).text(shortDate);
+                        // const temp = Math.floor(+response.list[i].main.temp)
                         $("#temp-" + index).text("Temp: "+response.list[i].main.temp);
                         $("#humid-" + index).text("Humidity: "+response.list[i].main.humidity);
                         $("#icon-" + index).attr('src', iconURL);
